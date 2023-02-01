e9124e3146564b812d78fb86c817b1beb5077c77
Jhondel D. Caranay <jhondeldelconacaranay@gmail.com>
2023-02-01 19:57:44 +0800

    add infininte scroll

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -3,22 +3,82 @@ import Link from "next/link";
 import { CreateTweet } from ".";
 import { api, RouterOutputs } from "../utils/api";
 import moment from "moment";
+import { useEffect, useState } from "react";
 
 type Tweets = RouterOutputs["tweet"]["timeline"]["tweets"];
 type Tweet = Tweets[number];
 
+function useScrollPosition() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    // const position = window.pageYOffset;
+    // setScrollPosition(position);
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const scrolled = (winScroll / height) * 100;
+
+    setScrollPosition(scrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return { scrollPosition, setScrollPosition };
+}
+
 const TimeLine = () => {
+  //  when scroll position is 100% fetch next page
+  const { scrollPosition, setScrollPosition } = useScrollPosition();
+
   const {
     data: tweetList,
     isLoading,
     error,
-  } = api.tweet.timeline.useQuery({
-    limit: 2,
-  });
+    hasNextPage,
+    hasPreviousPage,
+    fetchNextPage,
+    isFetching,
+  } = api.tweet.timeline.useInfiniteQuery(
+    {
+      limit: 2,
+    },
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage.nextCursor) {
+          return lastPage.nextCursor;
+        }
+      },
+    }
+  );
+  const tweets = tweetList?.pages.flatMap((page) => page.tweets);
+
+  //  when scroll position is 100% fetch next page
+  useEffect(() => {
+    if (scrollPosition === 100 && hasNextPage && !isFetching) {
+      // add debounce for scroll , 1000ms
+      const timer = setTimeout(() => {
+        fetchNextPage();
+        setScrollPosition(99);
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [scrollPosition, hasNextPage, isFetching, fetchNextPage]);
+  // console.log({ scrollPosition });
 
   if (isLoading) {
     console.log("loading");
-
     return <div>Loading...</div>;
   }
 
@@ -32,9 +92,16 @@ const TimeLine = () => {
       <CreateTweet />
       {/* tweets */}
       <div className="border-l-2 border-r-2 border-t-2 border-gray-500">
-        {tweetList?.tweets.map((tweet) => (
+        {tweets?.map((tweet) => (
           <Tweet key={tweet.id} tweet={tweet} />
         ))}
+
+        <button
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetching}
+        >
+          load next
+        </button>
       </div>
     </div>
   );
